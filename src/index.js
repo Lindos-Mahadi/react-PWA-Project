524e6180acda358f39d89361b140bd5898ca0fa4
lindo <lindo@LINDOS>
2021-01-31 21:32:48 +0600

    build up serviceWorker as public=sw.js and src=swDev.js

--- src/index.js
+++ src/index.js
@@ -2,21 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import swDev from './swDev';
 import {BrowserRouter} from "react-router-dom";
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle';
 import "../node_modules/font-awesome/css/font-awesome.min.css";
 
 ReactDOM.render(
-  <React.StrictMode>
+  // <React.StrictMode>
+  <React.Fragment>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </React.StrictMode>,
+    </React.Fragment>,
+  // </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+swDev();
